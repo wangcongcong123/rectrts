a27ab7ea6f9eb92b0b3d95bb7ff54a1133aea3dd
Congcong Wang <18810925338@163.com>
2018-11-16 16:22:08 +0000

    congcong
    from desktop

--- dataset/update.py
+++ dataset/update.py
@@ -16,19 +16,35 @@ cursor = db.cursor()
 #     tweetid=each[0]
 #     ft.write_to_log("tweets-to-update",tweetid+"\n")
 from tweepy import TweepError
-with open("log-tweets-to-update.txt") as f:
-    content=f.read()
-    tweetidstoupdate=content.split("\n")
-    count=0
-    for eachid in tweetidstoupdate:
-        status_text=""
+
+
+def updateRateLimit():
+    id_list = []
+    with open("log-get-full-tweet.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if "Rate limit" in line.strip():
+                id_list.append(line.strip().split("@")[0])
+    return id_list
+
+    # if __name__ == '__main__':
+    #     updateRateLimit()
+    #
+if __name__ == '__main__':
+    # with open("log-tweets-to-update.txt") as f:
+    # content = f.read()
+    # tweetidstoupdate = content.split("\n")
+    id_list=updateRateLimit()
+    count = 0
+    for eachid in id_list:
+        status_text = ""
         try:
             status = tweetapi.api.get_status(eachid, tweet_mode='extended')
         except TweepError as err:
             print(err)
             err_log = "\n" + eachid + "@TweepError:" + str(err)
             print(err.api_code)
-            ft.write_to_log("get-full-tweet",err_log)
+            ft.write_to_log("get-full-tweet", err_log)
             if err.api_code == None:
                 print("sleeping 10 minutes due to the rate limit")
                 time.sleep(10 * 60)
@@ -36,11 +52,11 @@ with open("log-tweets-to-update.txt") as f:
         status_json = status._json
         json_str = json.dumps(status._json)
         if "retweeted_status" in status_json:
-            status_text=status_json['retweeted_status']['full_text']
+            status_text = status_json['retweeted_status']['full_text']
         else:
-            status_text=status_json['full_text']
-        sql = "update status set text='%s' where id='%s'"%(status_text.replace("\'",""),eachid)
-        print("process", count," Tweet@",eachid)
+            status_text = status_json['full_text']
+        sql = "update status set text='%s' where id='%s'" % (status_text.replace("\'", ""), eachid)
+        print("process", count, " Tweet@", eachid)
         count += 1
         try:
             cursor.execute(sql)
@@ -54,10 +70,4 @@ db.close()
 
 #
 
-
-#
-
 # print(status)
-
-
-
