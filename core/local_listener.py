e7cf50094b57b64deb3074a3125b3f7e3b549315
Congcong Wang <18810925338@163.com>
2018-11-16 21:19:05 +0000

    congcong
    jaccard first run

--- core/local_listener.py
+++ core/local_listener.py
@@ -1,5 +1,6 @@
-from core.processor import Processor
+# from core.processor import Processor
 import time
+import datetime
 import pymysql
 
 import timeit
@@ -8,8 +9,16 @@ class LocalListener:
     cursor = db.cursor()
 
     def __init__(self):
-        pass
+        self.simulated_collection=[]
+        results=LocalListener.get_status_2017()
 
+        for each in results:
+            created_time = each[4]
+            timestamp_ = time.mktime(datetime.datetime.strptime(created_time, "%Y-%m-%d %H:%M:%S").timetuple())
+            rebuild_tweet = (each[0], created_time.split()[0].replace("-", ""), int(timestamp_), each[1])
+            self.simulated_collection.append(rebuild_tweet)
+        self.length_t=len( self.simulated_collection)
+    
     # this a status by id which is used for debugging sometimes
     def get_status_byId(self, id):
         sql = "select * from status where id = %s" % (id)
@@ -20,6 +29,17 @@ class LocalListener:
             self.db.rollback()
             self.db.close()
         return results
+
+    def get_status_byIdlist(self, id_list):
+        sql = "select * from status where id in %s" % (id_list)
+        try:
+            self.cursor.execute(sql)
+            results = self.cursor.fetchall()
+        except:
+            self.db.rollback()
+            self.db.close()
+        return results
+
 
     def get_train_status(self):
         pass
@@ -60,33 +80,55 @@ class LocalListener:
             self.db.close()
         return results
 
+    # select count(*) from status where dataset_src like "rts2017%"
+    @classmethod
+    def get_status_2017(cls):
+        sql = "select id,text,topic_label,relevance,created from status where dataset_src like %s"%("'"+"rts2017%"+"'")
+        results=()
+        try:
+            cls.cursor.execute(sql)
+            results = cls.cursor.fetchall()
+        except:
+            cls.db.rollback()
+            cls.db.close()
+        return results
+
     def get_status_by_topiclist(self, topiclist):
         results=()
         for topic in topiclist:
             results+=self.get_status_by_topic(topic)
         return results
 
+
     def listen(self, executor):
+        # results=self.get_status_2017()
+        # for each in results:
+        count = 0
+        for rebuild_tweet in self.simulated_collection:
+            count += 1
+            # print(rebuild_tweet)
+            executor.excute(rebuild_tweet)
+            print("Processing Tweet", "(", self.length_t, ")", count)
         # print("here is listenning local status")
-        with open("../dataset/log-tweetIds-in-db-2017-and-epoch.txt") as f:
-            lines=f.readlines()
-            count=0
-            length_t=len(lines)
-            for each in lines:
-                start = timeit.default_timer()
-                count+=1
-                columns=each.strip().split()
-                tweetid=columns[0]
-                tweet=self.get_status_byId(tweetid)
-                rebuild_tweet=(tweetid,columns[1],columns[2],tweet[0][6])
-                # time.sleep(1)
-                # print(rebuild_tweet)
-                #rebuild_tweet format:
-                #('891035114838294528', '20170728', '1501274300', 'Why are you like this???? @anakarendavilaa https://t.co/pbNHPb2MgO')
-                executor.excute(rebuild_tweet)
-                stop = timeit.default_timer()
-                # print('Time: ', stop - start)
-                print("Processing Tweet","(",length_t,")",count)
+        # with open("../dataset/log-tweetIds-in-db-2017-and-epoch.txt") as f:
+        #     lines=f.readlines()
+        #     count=0
+        #     length_t=len(lines)
+        #     for each in lines:
+        #         start = timeit.default_timer()
+        #         count+=1
+        #         columns=each.strip().split()
+        #         tweetid=columns[0]
+        #         tweet=self.get_status_byId(tweetid)
+        #         rebuild_tweet=(tweetid,columns[1],columns[2],tweet[0][6])
+        #         # time.sleep(1)
+        #         # print(rebuild_tweet)
+        #         #rebuild_tweet format:
+        #         #('891035114838294528', '20170728', '1501274300', 'Why are you like this???? @anakarendavilaa https://t.co/pbNHPb2MgO')
+        #         executor.excute(rebuild_tweet)
+        #         stop = timeit.default_timer()
+        #         # print('Time: ', stop - start)
+        #         print("Processing Tweet","(",length_t,")",count)
 
     def count(self):
         count_dic = {}
@@ -107,9 +149,19 @@ class LocalListener:
         #     else:
         #         count_dic[each[2]] += 1
         print(count_dic)
+
+
 if __name__ == '__main__':
     pass
-    # ll=LocalListener()
+    ll=LocalListener()
+    ll.listen()
+    # results=ll.get_status_byIdlist(str(("760265746425479168","891688738383953922"))+"")
+    # print(results)
+    # for each in results:
+    #     created_time=each[12]
+    #     timestamp_=time.mktime(datetime.datetime.strptime(created_time, "%Y-%m-%d %H:%M:%S").timetuple())
+    #     rebuild_tweet = (each[0], created_time.split()[0].replace("-",""), int(timestamp_), each[6])
+    #     print(rebuild_tweet)
     # ll.listen()
     # print(len(ll.get_status_by_topiclist(["RTS46","RTS47"])))
     # locallistener.count()
